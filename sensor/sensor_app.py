436e16f50a6b487f025d6a1f3bac262b224fcfa1
Ken Plowman <kenplowman@gmail.com>
2020-06-27 23:09:54 +1000

    module2

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -23,9 +23,9 @@ test_area = 1
 recs = house_info.get_data_by_area("id",rec_area=test_area)
 print("\nHouse sensor records for area{} = {}".format(test_area, len(recs)))
 
-test_date = datetime.strftime("5/9/20","%m/%d/%y")
+test_date = datetime.strptime("5/9/20","%m/%d/%y")
 recs = house_info.get_data_by_date("id",rec_date=test_date)
-print("\nHouse sensor records for date{} = {}".format(test_date.strftime("%m/%d/%y"),len(recs)))
+print("\nHouse sensor records for date{} = {}".format(test_date.strptime("%m/%d/%y"),len(recs)))
 
 # Module 3 code here:
 
