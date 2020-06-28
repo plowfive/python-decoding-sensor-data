a8d75005f68bdbcf96a8088d0067b85d3ab11b48
Ken Plowman <kenplowman@gmail.com>
2020-06-28 15:13:01 +1000

    Module3

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,7 @@
 from load_data import load_sensor_data
 from house_info import HouseInfo
+from temperature_info import TemperatureData
+
 from datetime import datetime, date
 
 # Runner script for all modules
@@ -29,7 +31,14 @@ print("\nHouse sensor records for date {} = {}".format(test_date.strftime("%m/%d
 
 
 # Module 3 code here:
+temperature_data = TemperatureData(data)
+recs = temperature_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Temperature Sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs),min(recs)))
 
+recs = temperature_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Temperature Sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs),min(recs)))
 
 
 
