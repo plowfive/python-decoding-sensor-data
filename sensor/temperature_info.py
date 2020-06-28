dbe86e9a9654f358f65dfa1cda4356017967c180
Ken Plowman <kenplowman@gmail.com>
2020-06-28 13:47:19 +1000

    module3

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -5,7 +5,6 @@ class TemperatureData(HouseInfo):
     def __init__(self, data, year=None):
         # super().__init__(data)
 
-    def _convert_data(self, data):
-        recs = []
+        def _convert_data(self, data):
+           recs = []
 
-        
