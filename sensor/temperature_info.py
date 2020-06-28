bb847cf89edc8bd7f42464a092e10f9d3a9cd6db
Ken Plowman <kenplowman@gmail.com>
2020-06-28 14:08:24 +1000

    Module3

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -10,8 +10,8 @@ class TemperatureData(HouseInfo):
     def _convert_data(self, data):
         recs = []
         for rec in data:
-            recs.append(int(rec),base=10)
+            recs.append(int(rec,base=10))
         return recs
-    
+
 
 
